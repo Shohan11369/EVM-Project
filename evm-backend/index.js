1eb8330995e985e2ead5532e235cdea63b114b7d
Shohan111369 <sarkershohan346@gmail.com>
2026-02-13 15:47:53 +0800

    solve fixed

--- evm-backend/index.js
+++ evm-backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import voterRoutes from "./src/routes/voterRoutes.js";
-import addressRoutes from "./src/routes/addressRoutes.js"; 
+import addressRoutes from "./src/routes/addressRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -15,9 +15,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/api/voter", voterRoutes);
-app.use("/api", addressRoutes); 
+app.use("/api", addressRoutes);
+
+app.get("/", (req, res) => {
+  res.send("EVM Backend is running ✅");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`),
 );
